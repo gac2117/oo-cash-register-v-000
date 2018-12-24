aa55a37873fd108331a0e02127039cda7dfac70d
Grace Choi <oiograceoio@gmail.com>
2018-12-24 04:11:04 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,23 @@
+require 'pry'
 
+class CashRegister
+  attr_accessor :total
+  @@items = []
+
+  def initialize(total = 0)
+    @total = total
+  end
+
+  def discount
+    self.total = 20
+  end
+
+  def add_item(title, price, quantity = 1)
+    @@items << title
+    @total += (price * quantity)
+  end
+
+  def apply_discount
+    @total = total * 0.8
+  end
+end
