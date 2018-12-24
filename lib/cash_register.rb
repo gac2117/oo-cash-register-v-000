6a15635b2e4e193a480ea6b637eb349b2e40c701
Grace Choi <oiograceoio@gmail.com>
2018-12-24 06:00:39 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,7 +18,7 @@ class CashRegister
   end
 
   def apply_discount
-    @total = self.total * 0.8
+    @total = self.total * 0.78
     puts "After the discount, the total comes to #{@total}"
   end
 end
