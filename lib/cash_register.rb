199a5001eb87cdde2c6ebd4832d79a4e2db5c47f
Grace Choi <oiograceoio@gmail.com>
2018-12-24 06:14:24 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,8 +10,12 @@ class CashRegister
   end
 
   def add_item(title, price, quantity = 1)
-    @@items << title
-    @total += (price * quantity)
+    self.total += price * quantity
+    quantity.times do
+      items << title
+    end
+    self.last_transaction = price * quantity
+    end
   end
 
   def apply_discount
